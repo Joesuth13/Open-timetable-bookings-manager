2e75b91322b4d74da082eb3b0b8147b2dcf8e25e
Samuel Sutherland <joesuth13@gmail.com>
2024-12-13 22:57:15 +0000

    Migrate to PostGreSQL

--- OpenClubManager/settings.py
+++ OpenClubManager/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,12 +77,11 @@ WSGI_APPLICATION = 'OpenClubManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
@@ -122,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'timetable.UserInfo'
--- timetable/models.py
+++ timetable/models.py
@@ -1,9 +1,17 @@
-from multiselectfield import MultiSelectField
+
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 # Create your models here.
+class UserInfo(AbstractUser):
+    
+    full_name = models.CharField(max_length=200)
+    contact_number = models.CharField(max_length=12)
+    ParticipantID = models.ForeignKey('Participant', on_delete=models.CASCADE)
 
-class ClassType (models.Model):
+
+class ClassType(models.Model):
     duration_choices = (('15 Minutes', 15), ('30 Minutes', 30),
                         ('45 Minutes', 45), ('1 Hour', 60),
                         ('1 Hour 15 Minutes', 75), ('1 Hour 30 Minutes', 90),
@@ -17,32 +25,41 @@ class ClassType (models.Model):
     name = models.CharField(max_length=100)
     age_group_min = models.IntegerField()
     age_group_max = models.IntegerField(default=99)
-    duration = models.integerfield(choices = duration_choices)
-    price = models.DecimalField(decimal_places=2)
+    duration = models.IntegerField(choices = duration_choices)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     defaultCapacity = models.IntegerField()
 
     class Timetable(models.Model):
         day_choices = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
         name = models.CharField(max_length=200)
         active = models.BooleanField(default=False)
-        included_days = MultiSelectField(day_choices)
+        selected_days = models.JSONField(
+        default=list,
+        help_text="Store selected days of the week as a JSON array (e.g., ['MON', 'TUE'])"
+    )
+
         startTime = models.TimeField()
         endTime = models.TimeField()
         notes = models.TextField(blank=True)
 
     class ClassInstance(models.Model):
-        class_type = models.ForeignKey('ClassType')
+        class_type = models.ForeignKey('ClassType', on_delete=models.CASCADE)
         instance_date = models.DateTimeField()
         start_time = models.DateTimeField()
         finish_time = models.DateTimeField()
         Capacity = models.IntegerField(0)
-        attendees = models.ManyToManyField(to='Booking')
+        attendees = models.ManyToManyField('Booking')
 
     class Booking(models.Model):
-        userID = models.ForeignKey('User')
-        participantID = models.ForeignKey('Participant')
-        instanceID = models.ForeignKey('ClassInstance')
+        userID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        participantID = models.ForeignKey('Participant', on_delete=models.CASCADE)
+        instanceID = models.ForeignKey('ClassInstance', on_delete=models.CASCADE)
         paid_or_member = models.BooleanField(default=False)
         created_on = models.DateTimeField(auto_now_add=True)
         updated_on = models.DateTimeField(auto_now=True, blank=True, null=True)
         active = models.BooleanField(default=True)
+
+    class Participant(models.Model):
+        name = models.CharField(max_length=200)
+        date_of_birth = models.DateField()
+        
